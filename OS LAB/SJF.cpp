e07b4e3fe2172fda3e8dd0ba5e5d811141a58024
Dharan <opdharanff@gmail.com>
2023-03-12 09:03:30 +0530

    OS Assignment

--- OS LAB/SJF.cpp
+++ OS LAB/SJF.cpp
@@ -4,57 +4,57 @@ struct process
 {
     int id;
     int bt;
-    int waiting_time;
-    int turnaround_time;
-    int completion_time;
+    int waiting_time=0;
+    int turnaround_time=0;
+    int completion_time=0;
 };
 bool compare(process p1, process p2)
 {
     return p1.bt < p2.bt;
 }
-void sjf(process processes[], int n)
+void sjf(process pro[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
         int min = i;
         for (int j = i + 1; j < n; j++)
         {
-            if (processes[j].bt < processes[min].bt)
+            if (pro[j].bt < pro[min].bt)
             {
                 min = j;
             }
         }
-        swap(processes[min], processes[i]);
+        swap(pro[min], pro[i]);
     }
     int total_time = 0;
     cout << "Process\tBurst Time\tWaiting Time\tTurnaround Time\tCompletion Time" << endl;
     for (int i = 0; i < n; i++)
     {
-        cout << processes[i].id << "\t" << processes[i].bt << "\t\t";
+        cout << pro[i].id << "\t" << pro[i].bt << "\t\t";
         int waiting_time = total_time;
         cout << waiting_time << "\t\t";
-        int turnaround_time = waiting_time + processes[i].bt;
+        int turnaround_time = waiting_time + pro[i].bt;
         cout << turnaround_time << "\t\t";
-        int completion_time = total_time + processes[i].bt;
+        int completion_time = total_time + pro[i].bt;
         cout << completion_time << endl;
-        total_time += processes[i].bt;
+        total_time += pro[i].bt;
     }
 }
 int main()
 {
     int n;
-    cout << "Enter the number of processes = ";
+    cout << "Enter the number of process = ";
     cin >> n;
-    process processes[n];
+    process pro[n];
     for (int i = 0; i < n; i++)
     {
-        cout << "Enter the burst time for process " << i + 1 << ": ";
-        cin >> processes[i].bt;
-        processes[i].id = i + 1;
-        processes[i].waiting_time = 0;
-        processes[i].turnaround_time = 0;
-        processes[i].completion_time = 0;
+        cout << "Enter the burst time for process " << i + 1 << " = ";
+        cin >> pro[i].bt;
+        pro[i].id = i + 1;
+        pro[i].waiting_time = 0;
+        pro[i].turnaround_time = 0;
+        pro[i].completion_time = 0;
     }
-    sjf(processes, n);
+    sjf(pro, n);
     return 0;
 }
